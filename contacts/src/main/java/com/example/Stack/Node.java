51aec1f9aa9381244daf9823b90a7ded2f9732f3
Adrián Rangel Suárez <42705200+developeration1@users.noreply.github.com>
2024-08-29 13:55:01 -0600

    first attempt

---
+++ contacts/src/main/java/com/example/Stack/Node.java
@@ -0,0 +1,32 @@
+package com.example.Stack;
+
+public class Node<E> {
+    private E data;
+
+    private Node<E> bottom;
+
+    public Node(E data, Node<E> bottom) {
+        this.data = data;
+        this.bottom = bottom;
+    }
+
+    public Node(E data) {
+        this.data = data;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public Node<E> getBottom() {
+        return bottom;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public void setBottom(Node<E> node) {
+        this.bottom = node;
+    }
+}
