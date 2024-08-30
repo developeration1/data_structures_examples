50e230d722766d1a1c17c8ba0ea91e1b29a81510
Adrián Rangel Suárez <arangelsuarez@hotmail.com>
2024-08-30 14:37:27 -0600

    reset

--- demo/src/main/java/com/example/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Main {
-    public static void main(String[] args) {
-        List<Contact> list = new List<>();
-        list.add(new Contact(1341234123, "Paco"));
-    }
-}
